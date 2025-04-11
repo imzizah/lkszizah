e23c66b771a0694a421db0dfc7bcfb628604e0a8
Azizah <iijaahh157@gmail.com>
2025-04-11 14:24:07 +0700

    add new file

--- lks/resources/views/layout/main.blade.php
+++ lks/resources/views/layout/main.blade.php
@@ -220,19 +220,7 @@
               <li class="nav-item">
                 <a href="{{ route('admin.dashboard') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard nn</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
+                  <p>Dashboard game</p>
                 </a>
               </li>
             </ul>
